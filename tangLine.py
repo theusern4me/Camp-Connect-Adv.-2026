4e5d2c602c387c0c846b68cf571af86ef4814250
toasternation <134112003+theusern4me@users.noreply.github.com>
2026-06-09 15:04:09 -0400

    fixed tangLine

--- tangLine.py
+++ tangLine.py
@@ -5,5 +5,5 @@ def findTangent(home, end, slope, imageSize):
     imageX , imageY = imageSize[0], imageSize[1] # imageSize is an array of two .shape values (x,y)
     homeX , homeY = home[0], home[1] # home is an array of two values (x,y) to dictate where the starting point is
     # end is one value (x) to dictate where the ending vertical is
-    endY = homeY + (slope * end)
-    return endY
+    endY = homeY - (slope * end)
+    return int(numpy.round(endY))
